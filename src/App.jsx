f00814f04a8e22f87d32e1c7043b865f8a160172
Dominik Ilja <104933651+Dominik-Ilja@users.noreply.github.com>
2022-07-01 09:04:15 -0400

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import "./App.css";
+import "./styles/App.scss";
+import "./styles/utilities.scss";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
